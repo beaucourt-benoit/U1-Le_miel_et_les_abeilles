877915264d90961b27097b7190f72ed15363cbca
Benoit <benoit.beaucourt@laplateforme.io>
2020-11-20 14:33:14 +0100

    Work in progress

--- U1_ProjectPool_HoneyBees.py
+++ U1_ProjectPool_HoneyBees.py
@@ -6,56 +6,52 @@ from numpy import genfromtxt
 import random 
 import copy 
 
+#Mélange le génome de start à stop
+def shuffle_slice(a, start, stop):
+    i = start
+#    while (i < stop-1):
+    for i in range(1, stop-1):
+        idx = random.randrange(i, stop)
+        a[i], a[idx] = a[idx], a[i]
+        i += 1
+
 #Le champs, les fleurs et la ruche
+fleurs = np.array([])
+absx = np.array([])
+ordy = np.array([])
+id = np.array([])
 
-Tableau = []
-x = []
-y = []
-id = []
+fleurs = genfromtxt('Flowers.csv', delimiter=',')
+fleurs[0] = [500, 500]
+r = [[500,500]]
+champ = np.concatenate((fleurs, r))
+n = len(fleurs)
 
-Tableau = genfromtxt('Flowers.csv', delimiter=',')
-#print(Tableau)
-
-Tableau[0] = [500, 500]
-
-n = len(Tableau)
-
-for i in range(n):
-    x.append(int(Tableau[i][0]))
-    y.append(int(Tableau[i][1]))
-    id.append(int(i))
-
-x.append(500)
-y.append(500)
-
-#Distance de la ruche à chacune des fleurs
-a = int()
-b = int()
-c = 0
-
-#Fonction Fitness du génome
-for i in range(50):
-    a = ([(a - b)**2 for a, b in zip(Tableau[0], Tableau[i])])[0] + ([(a - b)**2 for a, b in zip(Tableau[0], Tableau[i])])[1]
-    b = b + a
-    print(a)
-    print(b)
+print("fleurs \n", fleurs)
+print("len fleurs\n",len(fleurs))
+print("champ \n",champ)
+print("len champ",len(champ))
 
 
-#Individu "abeille", 51 gènes, gène 0 = dist ruche à fleur1, gène 51 = dist fleur50 à ruche
-#for i in range(1,100):
-#    i = random.shuffle(Tableau)
-#    print(i)
+for j in range(n):
+    absx = np.append(absx, int(fleurs[j][0]))
+    ordy = np.append(ordy, int(fleurs[j][1]))
+    id = np.append(id, (int(j)))
 
-#   print(Dist, "\n")
-print(Tableau, "\n")
-a = copy.deepcopy(Tableau)
-np.random.shuffle(a)
-print(a, "\n")
+absx = np.append(absx, 500)
+ordy = np.append(ordy, 500)
 
+# Essaim
+essaim = np.array([])
 
-
-
-#plt.plot(x, y)
-plt.scatter(x, y)
-plt.scatter(x[0], y[0], color="red") #La Ruche
-plt.show()
+for i in range(10):  
+    shuffle_slice(champ, 1, 51)
+    print("Champ avant le append :", champ)
+    print("Essaim avant le append :", essaim)
+    essaim = np.append(essaim, champ)
+    print("Champ après le append :", champ)
+    essaim = reshape(essaim, (52, -1))
+    print("Essaim après le append :", essaim)
+        
+essaim = reshape(essaim, (20, 52))
+print("Final essaim \n", essaim)
