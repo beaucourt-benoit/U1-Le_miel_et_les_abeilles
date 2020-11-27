b2b97494799921b78ce6f25b350cfbc4d1deeaf9
benoit.beaucourt <benoit.beaucourt@laplateforme.io>
2020-11-27 12:22:18 +0100

    raz - orienté objet

---
+++ les-abeilles.py
@@ -0,0 +1,47 @@
+# Projet le miel et les abeilles
+# Algorithme génétique
+
+import numpy as np
+
+class Fleurs:
+    #Classe représentant un champ de fleurs
+    def __init__(self):
+    nbre de fleur = len(fleurs)
+    fleurs = import fichier csv
+
+class abeille:
+    init parcours = position ruche + init genome + postion ruche
+    distance (pt1, pt2) = ((x1 - x2)**2 + (y1 - y2)**2)
+    fitness = for i in  (len(init parcours) - 1) faire distance (init parcours[i], init parcours[i+1])
+    mutation()
+
+class Essaim:
+    nbre abeille = input()
+    init essaim= for (nbre abeille) random (fleurs)
+        meetbee(list
+        abeilles
+        parents) pour
+        i in range(len(abeilles
+        parents) - 1) faire
+
+class mutation(abeille):
+
+
+class crossover(abeille):
+    crossover(abeille1, abeille2)
+            pour i in len(genome) faire
+            si genome.abeille1[i] ET genome.abeille2[i] are in genome.bb
+            alors genome.bb[i] = random.choice(fleurs - genome.bb)
+            si genome.abeille1[i]
+
+class Generation(essaim(ruche(champ))):
+    genome.bb = array([])
+
+class generation
+    numéro generation
+    best abeille
+    best fitness
+    moy en fitness
+    fitness essaim
+
+
