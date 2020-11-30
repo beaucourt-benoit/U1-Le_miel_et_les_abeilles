650573512c9212f36067a44dac5caf685e89919c
benoit.beaucourt <benoit.beaucourt@laplateforme.io>
2020-11-30 16:52:53 +0100

    One step at a time. Ajout de pandas et structure en dataframe

--- les-abeilles.py
+++ les-abeilles.py
@@ -6,21 +6,20 @@ from copy import deepcopy, copy
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-class Fleurs:
+#class Fleurs:
     #Classe représentant un champ de fleurs
-    def __init__(self):
-        self.flowers, self.nbrflrs, self.fetchflowers()
+#    def __init__(self):
+#        self.flowers, self.nbrflrs, self.fetchflowers()
 
     #Constructeur de Fleurs
-    def fetchflowers(self):
-        df = pd.read_csv('Flowers.csv')
-        subset = df[['x', 'y']]
-        flowers = [tuple(x) for x in subset.to_numpy()]
-        print('flowers', flowers)
-        nbrflrs = len(flowers)
-        print('nombre de fleurs', nbrflrs)
-        return flowers, nbrflrs
+#    def fetchflowers(self):
+#        df = pd.read_csv('Flowers.csv')
+#        subset = df[['x', 'y']]
+#        flowers = [tuple(x) for x in subset.to_numpy()]
+#        print('flowers', flowers)
+#        nbrflrs = len(flowers)
+#        print('nombre de fleurs', nbrflrs)
+#        return flowers, nbrflrs
 
 class Abeille:
     # Classe représentant une abeille
@@ -30,22 +29,28 @@ class Abeille:
         self.fitness = self.fitness()
 
     def printtest(self):
-        print(self.fitness)
+        print('genome', self.genome)
+        print('fitness', self.fitness)
         return
 
     def buildgenes(self):
+        df = pd.read_csv('Flowers.csv')
+        subset = df[['x', 'y']]
+        flowers = [tuple(x) for x in subset.to_numpy()]
         f = flowers
+#        print('flowers', f, '\n')
         genome = deepcopy(f)
         np.random.shuffle(genome)
-        parcours = np.insert(copy(genome), 0, (500, 500))
-        parcours = np.append(parcours, (500, 500))
-        print('Parcours', parcours)
-#        print()
+#        print('genome', genome, '\n')
+    #    parcours = np.insert(genome, 0, (500, 500)
+        parcours = [(500, 500)] + genome + [(500, 500)]
+#        parcours = parcours.reshape(52, 2)
+#        parcours = [tuple(x) for x in ]
+        print('Parcours', parcours, '\n')
         return genome, parcours
 
     #Calcul de la distance
     def distance(self, pt1, pt2):
-
         print()
         dist = (int((pt1[0] - pt2[0])**2) + int((pt1[1] - pt2[1])**2))
         return dist
@@ -55,35 +60,39 @@ class Abeille:
         fitness = 0
         for i in range(len(self.parcours) - 1):
             dist = self.distance(self.parcours[i], self.parcours[i+1])
-            print("distance = ", dist)
+            print("distance = ", dist, '\n')
             fitness = fitness + dist
+            print("fitness = ", fitness, '\n')
         return fitness
 
     #Dessiner le parcours d'une abeille
     def draw_bee(self):
+        absx = np.array([])
+        ordy = np.array([])
         for i in range(len(self.parcours)):
-            absx = np.array([])
-            ordy = np.array([])
             disttotal = 0
-            absx = np.append(absx, self.parcours[i, :, 0])
-            ordy = np.append(ordy, self.parcours[i, :, 1])
-            plt.plot(absx, ordy)
-            plt.scatter(absx, ordy)
-            plt.scatter(absx[0], ordy[0], color="red") #Dessine la position de la Ruche
-            plt.show()
+            absx = np.append(absx, int(self.parcours[i][0]))
+            ordy = np.append(ordy, int(self.parcours[i][1]))
+        plt.plot(absx, ordy)
+        plt.scatter(absx, ordy)
+        plt.scatter(absx[0], ordy[0], color="red") #Dessine la position de la Ruche
+        plt.show()
 
-class Essaim(Abeille):
-    # Classe représentant un essaim de N abeilles
+class Essaim:
+    #Classe représentant un essaim de N abeilles
     def __init__(self):
         self.nba = int(input("Nombre d'abeille :"))
         self.birate = int(input("Birthrate :"))
         self.murate = int(input("Mutation rate :"))
         self.mucoef = int(input("Mutation coefficient :"))
+        self.essaim = self.makeessaim()
 
-    def essaim(self):
-        self.essaim = np.array([])
+    def makeessaim(self):
+        essaim = list(tuple())
         for i in range(self.nba):
-            self.essaim = np.append(self.essaim, self.genome)
+            essaim = np.append(essaim, list(Abeille.buildgenes(self)))
+        print('essaim', essaim, '\n')
+        return essaim
 
     #choix des abeilles pour le crossover
     def meetbee(self, essaim):
@@ -117,3 +126,7 @@ class generation:
 
 maya = Abeille()
 maya.printtest()
+maya.draw_bee()
+
+nuee = Essaim()
+print('ESSAIM', nuee.essaim, '\n')
